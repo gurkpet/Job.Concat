bd79587efc4932bea12b943e361427ad4a964eff
gurkpet <gierkep@gmail.com>
2018-08-02 18:45:02 -0400

    handle enter merged

--- client/src/components/CreateJob.jsx
+++ client/src/components/CreateJob.jsx
@@ -39,8 +39,6 @@ class CreateJob extends React.Component {
     this.createNewJob = this.createNewJob.bind(this);
   }
 
-<<<<<<< HEAD
-=======
   /**
    * @description button that checks key press for enter and if it is it runs the function it is passed
    * @param { { } } event the event being checked for enter
@@ -53,7 +51,6 @@ class CreateJob extends React.Component {
       callback();
     }
   }
->>>>>>> added ability to process edit job app and create job app when pressing enter
   // Here is only close the modal when click the button.
   // Eventally, we should handle when click the outside of the modal, still close the modal.
   handleClose() {
@@ -62,12 +59,10 @@ class CreateJob extends React.Component {
   }
 
   // Follow the React Best Practice Design Patterns to dynamiclly updates the state
-<<<<<<< HEAD
   handleChange(event) {
     var target = event.target;
     var value = target.value;
     var name = target.name;
-
     this.setState({
       [name]: value,
     });
@@ -146,71 +141,18 @@ class CreateJob extends React.Component {
                   type="text"
                   name="name"
                   value={this.state.name}
-                  onChange={e => {
-                    this.handleChange(e);
-                  }}
+                  onChange={this.handleChange.bind(this)}
+                  onKeyUp={e => this.handleEnter(e, this.createNewJob.bind(this))}
                 />
               </div>
-
-=======
-  handleChange(event, callback) {
-    var target = event.target;
-    var value = target.value;
-    var name = target.name;
-    this.setState({
-      [name]: value,
-    });
-  }
-
-  // Event listener for create a new job, and bubble the event back to trigger the
-  // Ajax call from the main component.
-  // Eventually, we should have form validation for this part.
-  createNewJob() {
-    console.log(this.state);
-    this.props.onSubmit(this.state);
-  }
-
-  // JSX renders html elements
-  render() {
-    return (
-      <div>
-        <Dialog
-          open={!!this.props.view}
-          onClose={this.handleClose}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >
-          <DialogTitle id="alert-dialog-title">{'Create a new job'}</DialogTitle>
-
-          <DialogContent>
-            <DialogContentText id="alert-dialog-description">
-              <div>
-                <label>Company name:</label>
-                <input
-                  type="text"
-                  name="name"
-                  value={this.state.name}
-                  onChange={this.handleChange}
-                  onKeyPress={e => this.handleEnter(e, this.createNewJob.bind(this))}
-                  required
-                />
-              </div>
-
->>>>>>> added ability to process edit job app and create job app when pressing enter
               <div>
                 <label>Job title:</label>
                 <input
                   type="text"
                   name="title"
                   value={this.state.title}
-<<<<<<< HEAD
-                  onChange={e => {
-                    this.handleChange(e);
-                  }}
-=======
-                  onChange={this.handleChange}
-                  onKeyPress={e => this.handleEnter(e, this.createNewJob.bind(this))}
->>>>>>> added ability to process edit job app and create job app when pressing enter
+                  onChange={this.handleChange.bind(this)}
+                  onKeyUp={e => this.handleEnter(e, this.createNewJob.bind(this))}
                   required
                 />
               </div>
@@ -220,14 +162,8 @@ class CreateJob extends React.Component {
                   type="text"
                   name="website"
                   value={this.state.website}
-<<<<<<< HEAD
-                  onChange={e => {
-                    this.handleChange(e);
-                  }}
-=======
-                  onChange={this.handleChange}
-                  onKeyPress={e => this.handleEnter(e, this.createNewJob.bind(this))}
->>>>>>> added ability to process edit job app and create job app when pressing enter
+                  onChange={this.handleChange.bind(this)}
+                  onKeyUp={e => this.handleEnter(e, this.createNewJob.bind(this))}
                 />
               </div>
               <div>
@@ -236,14 +172,8 @@ class CreateJob extends React.Component {
                   type="email"
                   name="email"
                   value={this.state.email}
-<<<<<<< HEAD
-                  onChange={e => {
-                    this.handleChange(e);
-                  }}
-=======
-                  onChange={this.handleChange}
-                  onKeyPress={e => this.handleEnter(e, this.createNewJob.bind(this))}
->>>>>>> added ability to process edit job app and create job app when pressing enter
+                  onChange={this.handleChange.bind(this)}
+                  onKeyUp={e => this.handleEnter(e, this.createNewJob.bind(this))}
                 />
               </div>
               <div>
@@ -252,14 +182,8 @@ class CreateJob extends React.Component {
                   type="phone"
                   name="phone"
                   value={this.state.phone}
-<<<<<<< HEAD
-                  onChange={e => {
-                    this.handleChange(e);
-                  }}
-=======
-                  onChange={this.handleChange}
-                  onKeyPress={e => this.handleEnter(e, this.createNewJob.bind(this))}
->>>>>>> added ability to process edit job app and create job app when pressing enter
+                  onChange={this.handleChange.bind(this)}
+                  onKeyUp={e => this.handleEnter(e, this.createNewJob.bind(this))}
                 />
               </div>
               <div>
@@ -268,14 +192,8 @@ class CreateJob extends React.Component {
                   type="text"
                   name="recruiter"
                   value={this.state.recruiter}
-<<<<<<< HEAD
-                  onChange={e => {
-                    this.handleChange(e);
-                  }}
-=======
-                  onChange={this.handleChange}
-                  onKeyPress={e => this.handleEnter(e, this.createNewJob.bind(this))}
->>>>>>> added ability to process edit job app and create job app when pressing enter
+                  onChange={this.handleChange.bind(this)}
+                  onKeyUp={e => this.handleEnter(e, this.createNewJob.bind(this))}
                 />
               </div>
               <div>
@@ -284,14 +202,8 @@ class CreateJob extends React.Component {
                   type="date"
                   name="postDate"
                   value={this.state.postDate}
-<<<<<<< HEAD
-                  onChange={e => {
-                    this.handleChange(e);
-                  }}
-=======
-                  onChange={this.handleChange}
-                  onKeyPress={e => this.handleEnter(e, this.createNewJob.bind(this))}
->>>>>>> added ability to process edit job app and create job app when pressing enter
+                  onChange={this.handleChange.bind(this)}
+                  onKeyUp={e => this.handleEnter(e, this.createNewJob.bind(this))}
                 />
               </div>
               <div>
@@ -300,14 +212,8 @@ class CreateJob extends React.Component {
                   type="date"
                   name="appliedDate"
                   value={this.state.appliedDate}
-<<<<<<< HEAD
-                  onChange={e => {
-                    this.handleChange(e);
-                  }}
-=======
-                  onChange={this.handleChange}
-                  onKeyPress={e => this.handleEnter(e, this.createNewJob.bind(this))}
->>>>>>> added ability to process edit job app and create job app when pressing enter
+                  onChange={this.handleChange.bind(this)}
+                  onKeyUp={e => this.handleEnter(e, this.createNewJob.bind(this))}
                 />
               </div>
               <div>
@@ -316,14 +222,8 @@ class CreateJob extends React.Component {
                   type="date"
                   name="interviewedDate"
                   value={this.state.interviewedDate}
-<<<<<<< HEAD
-                  onChange={e => {
-                    this.handleChange(e);
-                  }}
-=======
-                  onChange={this.handleChange}
-                  onKeyPress={e => this.handleEnter(e, this.createNewJob.bind(this))}
->>>>>>> added ability to process edit job app and create job app when pressing enter
+                  onChange={this.handleChange.bind(this)}
+                  onKeyUp={e => this.handleEnter(e, this.createNewJob.bind(this))}
                 />
               </div>
               <div>
@@ -332,14 +232,8 @@ class CreateJob extends React.Component {
                   type="text"
                   name="coverLetterUrl"
                   value={this.state.coverLetterUrl}
-<<<<<<< HEAD
-                  onChange={e => {
-                    this.handleChange(e);
-                  }}
-=======
-                  onChange={this.handleChange}
-                  onKeyPress={e => this.handleEnter(e, this.createNewJob.bind(this))}
->>>>>>> added ability to process edit job app and create job app when pressing enter
+                  onChange={this.handleChange.bind(this)}
+                  onKeyUp={e => this.handleEnter(e, this.createNewJob.bind(this))}
                 />
               </div>
               <div>
@@ -348,14 +242,8 @@ class CreateJob extends React.Component {
                   type="text"
                   name="payRange"
                   value={this.state.payRange}
-<<<<<<< HEAD
-                  onChange={e => {
-                    this.handleChange(e);
-                  }}
-=======
-                  onChange={this.handleChange}
-                  onKeyPress={e => this.handleEnter(e, this.createNewJob.bind(this))}
->>>>>>> added ability to process edit job app and create job app when pressing enter
+                  onChange={this.handleChange.bind(this)}
+                  onKeyUp={e => this.handleEnter(e, this.createNewJob.bind(this))}
                 />
               </div>
               <div>
@@ -363,22 +251,11 @@ class CreateJob extends React.Component {
                 <select
                   name="state"
                   value={this.state.state}
-<<<<<<< HEAD
-                  onChange={e => {
-                    this.handleChange(e);
-                  }}
-                >
-                  <option value="">Please choose an option</option>
-                  <option value="interested">Interested</option>
-                  <option value="pending">Pending/Applied</option>
-                  <option value="interview">Interview</option>
-=======
-                  onChange={this.handleChange}
-                  onKeyPress={e => this.handleEnter(e, this.createNewJob.bind(this))}
+                  onChange={this.handleChange.bind(this)}
+                  onKeyUp={e => this.handleEnter(e, this.createNewJob.bind(this))}
                 >
                   <option value="">Please choose an option</option>
                   <option value="pending">Pending</option>
->>>>>>> added ability to process edit job app and create job app when pressing enter
                   <option value="offered">Offered</option>
                   <option value="rejected">Rejected</option>
                 </select>
