410bbe2b1a7469d1d98af7518f29e51107572ca9
gurkpet <gierkep@gmail.com>
2018-08-02 17:43:06 -0400

    added form submission on enter for login and signup

--- client/src/components/LoginSignUp.jsx
+++ client/src/components/LoginSignUp.jsx
@@ -41,30 +41,44 @@ class LoginSignUp extends React.Component {
     };
   }
 
+  /**
+   * @description button that checks key press for enter and if it is it runs the function it is passed
+   * @param { { } } event the event being checked for enter
+   * @param { Function } callback the function to run if enter was pressed
+   * 
+   * @return { undefined } undefined
+   */
+  handleEnter(event, callback) {
+    if (event.key === 'Enter') {
+      callback();
+    }
+  }
+
+
   /** *This function takes the inputted values of email and password field and sends a post request to the server to check if the username/password combination is valid. If valid then the userinfo is saved in main app state with isLoggedIn state toggling to true and the login modal closes.*/
-  logIntoAccount () {
+  logIntoAccount() {
     let requestData = {
       email: document.getElementsByClassName('email')[0].value,
       password: document.getElementsByClassName('password')[0].value
     }
 
     this.props.submitData('/login', requestData, ((response, err) => {
-        if (response.data.messageCode === 103 || response.data.messageCode === 104) {
-          this.setState({
-            nonExistentUser: true
-          })
-        } else {
-          this.props.updateUserInfo(response.data.firstName, response.data.lastName, response.data.userName, response.data.email, response.data._id)
-          this.props.isLoggedIn(true);
-          this.props.displayLoginSignup(false);
-          this.props.getJobData();
-        }
+      if (response.data.messageCode === 103 || response.data.messageCode === 104) {
+        this.setState({
+          nonExistentUser: true
+        })
+      } else {
+        this.props.updateUserInfo(response.data.firstName, response.data.lastName, response.data.userName, response.data.email, response.data._id)
+        this.props.isLoggedIn(true);
+        this.props.displayLoginSignup(false);
+        this.props.getJobData();
       }
+    }
     ))
   }
 
   /** *This function takes the inputted values of the registration fields, sends a post request to server to make a new entry in database for the new user. Also checks if email or username is already in use, if so displays message to client.*/
-  registerForAccount () {
+  registerForAccount() {
     let requestData = {
       firstName: document.getElementsByClassName('firstName')[0].value,
       lastName: document.getElementsByClassName('lastName')[0].value,
@@ -74,16 +88,16 @@ class LoginSignUp extends React.Component {
     }
 
     this.props.submitData('/signup', requestData, (response, err) => {
-        if (response.data.messageCode === 101 || response.data.messageCode === 102) {
-          this.setState({
-            alreadyExistingUser: true
-          })
-        } else {
-          this.props.updateUserInfo(requestData.firstName, requestData.lastName, requestData.userName, requestData.email,response.data._id)
-          this.props.isLoggedIn(true);
-          this.props.displayLoginSignup(false);
-        }
+      if (response.data.messageCode === 101 || response.data.messageCode === 102) {
+        this.setState({
+          alreadyExistingUser: true
+        })
+      } else {
+        this.props.updateUserInfo(requestData.firstName, requestData.lastName, requestData.userName, requestData.email, response.data._id)
+        this.props.isLoggedIn(true);
+        this.props.displayLoginSignup(false);
       }
+    }
     )
   }
 
@@ -107,91 +121,91 @@ class LoginSignUp extends React.Component {
             aria-labelledby="alert-dialog-title"
             aria-describedby="alert-dialog-description"
           >
-          <DialogTitle id="alert-dialog-title">
-            {"Log into your account"}
-          </DialogTitle>
+            <DialogTitle id="alert-dialog-title">
+              {"Log into your account"}
+            </DialogTitle>
 
-          <DialogContent>
-            <DialogContentText id="alert-dialog-description">
-              <React.Fragment>
-                {MessageToUser}
-              </React.Fragment>
-              <React.Fragment>
-                Email:
-                <input type="text" className="email" name="email" required/>
-              </React.Fragment>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                <React.Fragment>
+                  {MessageToUser}
+                </React.Fragment>
+                <React.Fragment>
+                  Email:
+                <input onKeyPress={(e) => this.handleEnter(e, this.logIntoAccount.bind(this))} type="text" className="email" name="email" required />
+                </React.Fragment>
 
-              <React.Fragment>
-                Password:
-               <input type="password" className="password" name="password" required/>
-              </React.Fragment>
+                <React.Fragment>
+                  Password:
+               <input onKeyPress={(e) => this.handleEnter(e, this.logIntoAccount.bind(this))} type="password" className="password" name="password" required />
+                </React.Fragment>
 
 
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={() => this.logIntoAccount()} color="primary">
-              Log in
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={() => this.logIntoAccount()} color="primary">
+                Log in
             </Button>
-            <Button onClick={() => this.props.displayLoginSignup(false)} color="primary" autoFocus>
-              Exit
+              <Button onClick={() => this.props.displayLoginSignup(false)} color="primary" autoFocus>
+                Exit
             </Button>
-          </DialogActions>
-        </Dialog>
-      </React.Fragment>
-        )
+            </DialogActions>
+          </Dialog>
+        </React.Fragment>
+      )
     }
 
     if (this.props.view === 'register') {
       return (
-       <React.Fragment>
-        <Dialog
-          open={!!this.props.view}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >
-          <DialogTitle id="alert-dialog-title">{"Register for a new account"}</DialogTitle>
-          <DialogContent>
-            <DialogContentText id="alert-dialog-description">
+        <React.Fragment>
+          <Dialog
+            open={!!this.props.view}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">{"Register for a new account"}</DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
 
-              <React.Fragment>
-                First Name:
-                <input type="text" className="firstName" name="firstName" required/>
-              </React.Fragment>
+                <React.Fragment>
+                  First Name:
+                <input type="text" onKeyPress={(e) => this.handleEnter(e, this.registerForAccount.bind(this))} className="firstName" name="firstName" required />
+                </React.Fragment>
 
-              <React.Fragment>
-                LastName:
-                <input type="text" className="lastName" name="lastName" required/>
-              </React.Fragment>
+                <React.Fragment>
+                  LastName:
+                <input type="text" onKeyPress={(e) => this.handleEnter(e, this.registerForAccount.bind(this))} className="lastName" name="lastName" required />
+                </React.Fragment>
 
-              <React.Fragment>
-                Username:
-                <input type="text" className="userName" name="userName" required/>
-              </React.Fragment>
+                <React.Fragment>
+                  Username:
+                <input type="text" onKeyPress={(e) => this.handleEnter(e, this.registerForAccount.bind(this))} className="userName" name="userName" required />
+                </React.Fragment>
 
-              <React.Fragment>
-                Email:
-               <input type="text" className="email" name="email" required/>
-              </React.Fragment>
+                <React.Fragment>
+                  Email:
+               <input type="text" onKeyPress={(e) => this.handleEnter(e, this.registerForAccount.bind(this))} className="email" name="email" required />
+                </React.Fragment>
 
-              <React.Fragment>
-                Password:
-               <input type="password" className="password" name="password" required/>
-              </React.Fragment>
+                <React.Fragment>
+                  Password:
+               <input onKeyPress={(e) => this.handleEnter(e, this.registerForAccount.bind(this))} type="password" className="password" name="password" required />
+                </React.Fragment>
 
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={() => this.registerForAccount()} color="primary">
-              Register
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={() => this.registerForAccount()} color="primary">
+                Register
             </Button>
-            <Button onClick={() => this.props.displayLoginSignup(false)} color="primary" autoFocus>
-              Exit
+              <Button onClick={() => this.props.displayLoginSignup(false)} color="primary" autoFocus>
+                Exit
             </Button>
-          </DialogActions>
-        </Dialog>
-      </React.Fragment>
-        )
+            </DialogActions>
+          </Dialog>
+        </React.Fragment>
+      )
     }
   }
 }
